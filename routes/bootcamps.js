cdb0adb8e1b6a295d76295357d9a08359859d3d8
raut008 <anandraut0311@gmail.com>
2023-09-09 12:47:00 +0530

    Create a CRUD API of Bootcamp

---
+++ routes/bootcamps.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getBootcamp,
+  getBootcamps,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} = require("../controllers/bootcamps");
+
+router.route("/").get(getBootcamps).post(createBootcamp);
+
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .delete(deleteBootcamp)
+  .put(updateBootcamp);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,38 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const { connectDB } = require("./config/db");
 
-dotenv.config({path: './config/config.env'});
+// Route files
+const bootcamps = require("./routes/bootcamps");
+
+dotenv.config({ path: "./config/config.env" });
+
+connectDB();
 
 const app = express();
 
+//Body parser
+app.use(express.json());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server is running ${process.env.NODE_ENV} mode on port ${PORT}`))
+const server = app.listen(
+  PORT,
+  console.log(`Server is running ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
+//handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+
+  server.close(() => process.exit(1));
+});
